1ee497f05457b385c30da77a4dcc1d41f7941f2d
ethan <eidrory@massart.edu>
2019-12-01 23:44:10 -0500

    ..

--- scr/config/systemConfig.js
+++ scr/config/systemConfig.js
@@ -28,14 +28,14 @@ exports.systemConfigTemplate = {
       }
      ],
     devices: [
-       { deviceID: "Bottom-Sensor", deviceTYPE:"thermometer", devicePIN: 8, deviceNODE:"veg_tent", controller: "DS18B20", freq: 9000000, color: "rgba(65, 124, 211, 1)"}},
-       { deviceID: "Top-Sensor", deviceTYPE:"thermometer", devicePIN: 7, deviceNODE:"veg_tent", controller: "DS18B20", freq: 9000000, color: "rgba(188, 231, 132, 1)"},
+       { deviceID: "Bottom-Sensor", deviceTYPE:"thermometer", devicePIN: 8, deviceNODE:"veg_tent", controller: "DS18B20", freq: 9000000, color: "rgba(65, 124, 211, .7)"}},
+       { deviceID: "Top-Sensor", deviceTYPE:"thermometer", devicePIN: 7, deviceNODE:"veg_tent", controller: "DS18B20", freq: 9000000, color: "rgba(188, 231, 132, .7)"},
 
-       { deviceID: "window", deviceTYPE:"thermometer", devicePIN: 7, deviceNODE:"room", controller: "DS18B20", freq: 9000000, color: "rgba(147, 29, 86, 1)"},
-       { deviceID: "center_room", deviceTYPE:"thermometer", devicePIN: 8, deviceNODE:"room", controller: "DS18B20", freq: 9000000, color: "rgba(221, 211, 73, 1)"},
+       { deviceID: "window", deviceTYPE:"thermometer", devicePIN: 7, deviceNODE:"room", controller: "DS18B20", freq: 9000000, color: "rgba(147, 29, 86, .7)"},
+       { deviceID: "center_room", deviceTYPE:"thermometer", devicePIN: 8, deviceNODE:"room", controller: "DS18B20", freq: 9000000, color: "rgba(221, 211, 73, .7)"},
 
-       { deviceID: "Bottom_Sensor", deviceTYPE:"thermometer", devicePIN: 7, deviceNODE:"flowerTent", controller: "DS18B20", freq: 9000000, color: "rgba(74, 64, 153, 1)"},
-       { deviceID: "Top_Sensor", deviceTYPE:"thermometer", devicePIN: 8, deviceNODE:"flowerTent", controller: "DS18B20", freq: 9000000,color: "rgba(13, 6, 48, 1)"},
+       { deviceID: "Bottom_Sensor", deviceTYPE:"thermometer", devicePIN: 7, deviceNODE:"flowerTent", controller: "DS18B20", freq: 9000000, color: "rgba(74, 64, 153, .7)"},
+       { deviceID: "Top_Sensor", deviceTYPE:"thermometer", devicePIN: 8, deviceNODE:"flowerTent", controller: "DS18B20", freq: 9000000,color: "rgba(13, 6, 48, .7)"},
     ],
     timers: [
     ] // list of all devices in the system
