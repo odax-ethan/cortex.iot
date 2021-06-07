76bf0b24387ad13d4f6ded52b76a53ae278d083b
ethan <ethan.drory@gmail.com>
2021-06-07 15:46:25 -0400

    early_build out of new viewer

--- source/util/timestamp.js
+++ source/util/timestamp.js
@@ -1,33 +1,4 @@
 var dateFormat = require('dateformat');
-//fuction for standardized time - let people set the clock to there needs
-// assumes you are calling class 
-// class timestamp {
-//     constructor(UTC_offset) {
-//         this.UTC_offset = UTC_offset;
-//     }
-
-//     getTime(){
-//        return new Date();  
-//     }
-
-//     get UTC_Offset_Milli (){
-//         let d = this.getTime()
-//         return d.getTimezoneOffset() * 60000
-//     }
-
-//     // local offset
-//     get local(){
-//         let minuteToMilli = 60000
-//         let hourToMilli = 3600000
-//         let d = this.getTime()+ this.UTC_Offset_Milli
-//         let offset = this.UTC_offset
-//         // let utc =  d.getTime() + (d.getTimezoneOffset() * minuteToMilli);
-//         var nd = new Date(d);
-//         return dateFormat(nd, "dd-mm-yyyy HH:MM:ss");
-//     }
-
-//   }
-
 
 class timestamp {
     constructor (time_zone){
@@ -37,17 +8,15 @@ class timestamp {
     // stupid and simple
     getTime(){
         var date = new Date(); 
-        var now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-         date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
-
-        return new Date(now_utc);
+        var current_location_local_time =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+        date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+        return  new Date(current_location_local_time).toISOString(); 
     }
 
     get local() {
         return this.getTime().toString()
     }
 }
-
 
 
 TIMESTAMP = new timestamp(process.env.TIMEZONE)
